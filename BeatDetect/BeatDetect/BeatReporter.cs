766d8489f367bfddb6c0c0eddc46b18c5bc7738c
Tim Boode <tboode@everyangle.org>
2016-12-14 13:24:02 +0100

    BeatDetect app added, required node firmware added

---
+++ BeatDetect/BeatDetect/BeatReporter.cs
@@ -0,0 +1,68 @@
+﻿using BeatDetectorCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BeatDetect
+{
+    class BeatReporter
+    {
+        private BeatDetector detector;
+        private static BeatReporter myself;
+        private TimeStamp lastBeat;
+        private UInt64 amountOfBeats = 0;
+
+        private BeatReporter(BeatDetector detector)
+        {
+            this.detector = detector;
+        }
+
+        public static BeatReporter GetReporterInstance(BeatDetector detector)
+        {
+            return myself ?? (myself = new BeatReporter(detector));
+        }
+
+        //public Task GetDetectionTask()
+        //{
+            
+        //}
+
+        public void DetectAndReportBeats()
+        {
+            detector.setStarted(true);
+
+            while (true)
+            {
+                detector.update();
+
+                lastBeat = detector.getLastBeat();
+
+                //I'm sure no song has a bpm higher than 6000 so 
+                //it's safe to sleep 10ms before checking if a new beat has been detected
+                //This will also save a lot of processing power.
+                Thread.Sleep(10);
+                if(detector.getLastBeat() != null && lastBeat != null)
+                {
+                    if (lastBeat.getMilliseconds() != detector.getLastBeat().getMilliseconds())
+                        reportLastBeat();
+                }
+            }
+        }
+
+        private void reportLastBeat()
+        {
+            amountOfBeats++;
+            if (amountOfBeats%2 == 0)
+            {
+                Console.WriteLine("Beat");
+            }
+            else
+            {
+                Console.WriteLine("                Beat");
+            }
+        }
+    }
+}
---
+++ BeatDetect/BeatDetect/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BeatDetectorCSharp;
+
+namespace BeatDetect
+{
+    public partial class Form1 : Form
+    {
+        BeatDetector detector = BeatDetector.Instance();
+        private BeatReporter reporter;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            detector.loadSystem();
+            //electronic music most of the time needs a sample size of 2048
+            //music like rage against the machine works best with a sample size of 256 but still barely works
+            detector.LoadSong(256, "ratm.mp3");
+            
+            reporter = BeatReporter.GetReporterInstance(detector);
+            Console.WriteLine("Instance initialized and loaded!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            reporter.DetectAndReportBeats();
+        }
+    }
+}
